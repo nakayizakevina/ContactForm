15455cdb2e5aad67f054e4e0bfd6c644c2017661
Nakayiza Kevina <kevinanakayiza@gmail.com>
2026-02-09 12:07:40 +0300

    Added a preview form

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const Form = document.getElementById("form")
 const FirstName = document.getElementById("first_name");
 const LastName = document.getElementById("last_name");
 const Email = document.getElementById("email");
+const Message = document.getElementById("message")
+
+
+const FirstNamePreview = document.getElementById("firstNamePreview");
+const LastNamePreview = document.getElementById("lastNamePreview");
+const EmailPreview = document.getElementById("emailPreview");
+const MessagePreview = document.getElementById("messagePreview")
 
 const EmailErrorState = document.getElementById("email__error")
 
@@ -17,19 +24,28 @@ function UserEmailPattern(useremail){
     return EmailPattern.test(useremail);
 }
 
+function UserMessage(e){
+  const mesage = "";
+}
+
 Form.addEventListener("submit", function (e) {
      e.preventDefault()
 
   const ValidFirstName= UserNamePattern(FirstName.value);
   const ValidLastName = UserNamePattern(LastName.value);
   const ValidEmail= UserEmailPattern(Email.value);
+  const message = UserMessage(Message.value);
 
   if (ValidFirstName && ValidLastName && ValidEmail) {
     alert("Message Sent! Thanks for completing the form. We'll be in touch soon!");
   } 
-  // else {
-  //   alert("Please check your inputs and try again.");
-  // }
+
+  FirstNamePreview .textContent = FirstName.value;
+  LastNamePreview.textContent = LastName.value;
+  EmailPreview.textContent = Email.value;
+  MessagePreview.textContent = Message.value;
+
+
 });
 
 function CheckEmailState(){
